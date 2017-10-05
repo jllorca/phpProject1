7122f00760998f1236d8097cf08e569e746f6e03
Joan <llorca_26@hotmail.com>
2017-10-05 20:32:31 +0200

    confirmacion del ejer 6

---
+++ formulario.php
@@ -0,0 +1,3 @@
+<?php 
+ 	echo '<p>Introduce tu nombre:</p>';
+ ?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+ <head>
+  <title>Prueba de PHP</title>
+ </head>
+ <body>
+ <?php 
+ 	echo '<p>Hola Mundo</p>';
+ 	$cont = file_get_contents("formulario.php"); 
+ 	echo $cont;
+ 	echo 'Sumo dos variables y realizo la operacion, que es:'
+ 	$v1=5;
+ 	$v2=2;
+ 	
+ 	$operacio=$v1/$v2;
+
+ 	echo $operacio;
+
+
+ ?>
+ </body>
+</html>
